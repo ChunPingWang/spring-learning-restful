6ace3c976111b18fa76afb0597913f9b4619199c
Rex Wang <cping.wang@gmail.com>
2025-02-02 00:24:43 +0800

    added the GET method of RESTful

---
+++ src/main/java/com/example/restfulApi/BookStoreController.java
@@ -0,0 +1,21 @@
+package com.example.restfulApi;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/bookstore")
+public class BookStoreController {
+
+   @GetMapping("/{bookId}")
+    public ResponseEntity<Book> getBookById(@PathVariable Long bookId) {
+        // 暫時這樣寫，後續再重構從資料庫查詢
+         if(bookId==1111L)
+             return ResponseEntity.ok(new Book("Java Programming", 100.0));
+         else
+             return ResponseEntity.notFound().build();
+    }
+}
---
+++ src/test/java/com/example/restfulApi/BookStoreApplicationTests.java
@@ -0,0 +1,31 @@
+package com.example.restfulApi;
+
+import org.junit.jupiter.api.Test;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+public class BookStoreApplicationTests {
+    @LocalServerPort
+    private int port;
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Test
+    void shouldReturnABook() throws Exception {
+        ResponseEntity<Book> response = this.restTemplate.getForEntity("http://localhost:" + port + "/bookstore/1111", Book.class);
+        // 即將廢棄
+        assertThat(response.getStatusCodeValue()).isEqualTo(200);
+        // 新寫法
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+    }
+}
