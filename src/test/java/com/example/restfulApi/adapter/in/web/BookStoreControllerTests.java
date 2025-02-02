57c77be9b6eb917399e3193d7431fa588ba13e64
Rex Wang <cping.wang@gmail.com>
2025-02-03 00:21:49 +0800

    added a test case and modified the url

--- src/test/java/com/example/restfulApi/adapter/in/web/BookStoreControllerTests.java
+++ src/test/java/com/example/restfulApi/adapter/in/web/BookStoreControllerTests.java
@@ -7,7 +7,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
@@ -15,18 +14,24 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 public class BookStoreControllerTests {
-    @LocalServerPort
-    private int port;
 
     @Autowired
     private TestRestTemplate restTemplate;
 
     @Test
     void shouldReturnABook() throws Exception {
-        ResponseEntity<Book> response = this.restTemplate.getForEntity("http://localhost:" + port + "/bookstore/1111", Book.class);
+        ResponseEntity<Book> response = this.restTemplate.getForEntity("/bookstore/1111", Book.class);
         // 即將廢棄
         assertThat(response.getStatusCodeValue()).isEqualTo(200);
         // 新寫法
         assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+    }
+
+    @Test
+    void shouldNotReturnABookWithAnUnknownId() throws Exception {
+        ResponseEntity<Book> response = this.restTemplate.getForEntity("/bookstore/9999", Book.class);
+        // 新寫法
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.NOT_FOUND);
+        assertThat(response.getBody()).isNull();
     }
 }
